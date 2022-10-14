48992f80c921db0664cb7f30b34491fce9459bd9
Ablaze789_ALX <anthonychinaza9@gmail.com>
2022-10-13 17:56:25 -0700

    Update

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * 	main - Entry point
+ *
+ * 	Description: 'This prints the size of different data type'
+ *
+ * 	Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	sizeof(char);
+	sizeof(int);
+	sizeof(long int);
+	sizeof(long long int);
+	sizeof(float);
+	return (0);
+}
