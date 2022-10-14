3e3665dc0a09879ace6a3ca86e57b9e9e9cd49ef
Ablaze789_ALX <anthonychinaza9@gmail.com>
2022-10-13 17:03:35 -0700

    Update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
+/*
+ *
+ *	main - Entry point
+ *
+ *
+ *
+ *	Return: Always 0 (Success)
+ *
+ *     */
 
-
-int main(void) {
-	puts("\"Programming is like building a multilingual puzzle\n");
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
