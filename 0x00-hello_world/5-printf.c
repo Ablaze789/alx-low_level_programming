42dc8ba6227e8a5d6b1627c6f735922e87f75500
Ablaze789_ALX <anthonychinaza9@gmail.com>
2022-10-13 17:21:51 -0700

    Update

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *	main - Entry point
+ *
+ *	Description: 'This programme orints out a string using the function printf'
+ *
+ *	Result: Always 0 (Success)
+ */
+
+
+
+int main(void)
+
+{
+
+	    printf("with proper grammar, but the outcome is a piece of art,");
+	   
+	    return(0);
+
+}
