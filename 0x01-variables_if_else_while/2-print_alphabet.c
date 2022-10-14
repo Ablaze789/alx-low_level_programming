e1e90ad30214d93dcef44d4fab14d59a0d1187ae
Ablaze789_ALX <anthonychinaza9@gmail.com>
2022-10-14 12:48:12 -0700

    Update

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*
+ *	main - Entry point
+ *
+ *	Description: 'This prints lowercase letters'
+ *
+ *	Return: Always 0
+ */
+
+int main(void)
+{
+	int letter;
+
+	for (letter = 'a', letter <= 'z', letter++)
+		putchar(letter);
+	putchar("\n");
+	return (0);
+}
