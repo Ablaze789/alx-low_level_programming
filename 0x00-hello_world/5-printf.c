8e60aedeef2257fd9bc64e651d089e2396ef1bbf
Ablaze789_ALX <anthonychinaza9@gmail.com>
2022-10-13 18:51:10 -0700

    Update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -10,11 +10,7 @@
 
 int main(void)
 {
-	char a;
-
-	a = "with proper grammar, but the outcome is a piece of art,";
-
 	/* Displays the string inside quotations */
-	printf("%c\n", a);
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
