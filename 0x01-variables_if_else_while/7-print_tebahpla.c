06ffc31587510b04abbd9e44239785b410a9767b
Anthony <75227567+Ablaze789@users.noreply.github.com>
2022-10-16 23:46:09 +0100

    Create 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * 
+ * Description: 'This print lowercase alphabet in reverse order'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char rev;
+
+	for (rev = 'z'; rev >= 'a'; rev--)
+        putchar(rev);
+	putchar('\n');
+	return (0);
+}
