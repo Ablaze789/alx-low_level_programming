90af447192107a98a6626298768d71d9d47748e8
Anthony <75227567+Ablaze789@users.noreply.github.com>
2022-10-16 23:50:45 +0100

    Create 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *	main - Entry point
+ *
+ *	Description: 'This prints letters of base 16'
+ *
+ *	Return: Always 0
+ */
+
+int main(void)
+{
+	int num;
+    char letter;
+
+    for (num = '0'; num <= '9'; num++)
+		putchar(num);
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
