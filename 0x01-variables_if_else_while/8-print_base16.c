3ab0e66bcc61b82de836b70a7c70cb468e43b0b7
Anthony <75227567+Ablaze789@users.noreply.github.com>
2022-10-17 00:01:58 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,9 +11,9 @@
 int main(void)
 {
 	int num;
-    char letter;
+	char letter;
 
-    for (num = '0'; num <= '9'; num++)
+	for (num = '0'; num <= '9'; num++)
 		putchar(num);
 	for (letter = 'a'; letter <= 'f'; letter++)
 		putchar(letter);
