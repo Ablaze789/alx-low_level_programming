b5b63a2263a7f72d025768cdda04da655ec568d9
Anthony <75227567+Ablaze789@users.noreply.github.com>
2022-10-16 23:14:20 +0100

    Create 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * 
+ * Description: 'This print lowercase alphabet with a few exception'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		if (i != 'e' && i != 'q')
+            putchar(i);
+	putchar('\n');
+	return (0);
+}
