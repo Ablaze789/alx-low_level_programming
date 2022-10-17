276b535f8fcb7e82a8e526727d41812eda1a2257
Anthony <75227567+Ablaze789@users.noreply.github.com>
2022-10-17 15:52:20 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,21 +2,25 @@
 
 /**
  * main - Entry point
- *
- * Description: 'This code prints all the possible combination of single digit numbers'
  * 
+ * Description: 'This code prints all the possible combination of single digit numbers'
+ *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int single;
+	int num;
 
-	for (single = '0'; single <= '9'; single++)
-		putchar(single);
-		if (single != '9')
-			putchar(', ');
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+		if (num != '9')
+		{
+			putchar(',');
 			putchar(' ');
+		}
+	}
 	putchar('\n');
 
 	return (0);
