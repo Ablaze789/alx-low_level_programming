836aea9216b870dcb9f2240625de4a1a850a46d9
Ablaze789_ALX <anthonychinaza9@gmail.com>
2022-10-18 03:37:56 -0700

    Create

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *	main -Entry point
+ *
+ *	Description: 'This is a print code'
+ *
+ *	Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
