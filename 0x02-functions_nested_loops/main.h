96b0ad23f0203f0e5aeae22d416539c20668b155
Ablaze789_ALX <anthonychinaza9@gmail.com>
2022-10-19 11:49:53 -0700

    Update

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,17 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
-void _putchar(char x)
-{
-    putchar(x);
-}
-void print_alphabet(void)
-{
-    char i;
-    for (i = 'a'; i <= 'z'; i++)
-    {
-        _putchar(i);
-    }
-    _putchar('\n');
-}
+void _putchar(char x);
 #endif
