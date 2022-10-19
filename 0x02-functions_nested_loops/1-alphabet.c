02392b5acf2fde7d123b44e1bedbcf76c14c6d6a
Anthony <75227567+Ablaze789@users.noreply.github.com>
2022-10-19 18:19:29 +0100

    Create 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *	main -Entry point
+ *
+ *	Description: 'This is a print code'
+ *
+ *	Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+    print_alphabet();
+    return (0);
+}
