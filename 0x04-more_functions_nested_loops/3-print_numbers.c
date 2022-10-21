3d10c457d25a2eac054594fcbe39c9e63d851ba7
Ablaze789_ALX <anthonychinaza9@gmail.com>
2022-10-21 03:24:08 -0700

    Update

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,18 +2,19 @@
 #include "main.h"
 
 /**
-* print_most_numbers - This prints the mumber 0-9 with the exception of 2 and 4
-* Return: void
+*	print_most_numbers - This prints the mumber 0-9 with the exception of 2 and 4
+*	Return: void
 */
 
 void print_most_numbers(void)
 {
-  int i;
-  for (i = 0; i <= 9; i++)
-  {
-    if (i != 2 || i != 4)
-    {
-      printf("%d", i);
-    }
-  }
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 || i != 4)
+		{
+			printf("%d", i);
+		}
+	}
 }
