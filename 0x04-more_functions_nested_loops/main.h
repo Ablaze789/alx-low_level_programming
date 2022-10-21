06e99be7ed214e367a29738e616d4356cb4baa72
Anthony <75227567+Ablaze789@users.noreply.github.com>
2022-10-21 09:58:38 +0100

    Update main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,22 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
 #include <ctype.h>
-extern int _putchar(char c)
-{
-  putchar(c); 
-}
-extern int _isupper(int c)
-{
-  int a;
-  if (isupper(c))
-  {
-    a = 1;
-    putchar(a);
-  }else{
-    a = 0;
-    putchar(a);
-  }
-}
-  int 
-#endif
+
+int _putchar(char c);
+
+#endif /*MAIN_H*/
