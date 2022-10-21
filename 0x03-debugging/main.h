78c080fc805b385b6fc2d3283de748ad8fc84bcf
Anthony <75227567+Ablaze789@users.noreply.github.com>
2022-10-21 09:05:41 +0100

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -7,7 +7,7 @@
  *	Description: 'This is the header file'
  */
 
-void positive_or_negative(int i)
-int largest_number(int a, int b, int c)
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
 
 #endif /*MAIN_H*/
