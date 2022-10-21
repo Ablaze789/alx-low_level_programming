dc7013f31ad0118bdb2d3e983c8b20c43e543094
Anthony <75227567+Ablaze789@users.noreply.github.com>
2022-10-21 12:01:35 +0100

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- *	main - tests function that prints if integer is positive or negative
- *	Description: 'This is a C program'
+ *	positive_or_negative - function that prints if integer is positive or negative
+ *	@i: input integer
  *	Return: 0
  */
 
