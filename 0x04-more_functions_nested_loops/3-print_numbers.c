02643ef6726b7d74ad8cdc53c04f323d3e12a9a6
Anthony <75227567+Ablaze789@users.noreply.github.com>
2022-10-21 11:16:20 +0100

    Update 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_most_numbers - This prints the mumber 0-9 with the exception of 2 and 4
+* Return: void
+*/
+
+void print_most_numbers(void)
+{
+  int i;
+  for (i = 0; i <= 9; i++)
+  {
+    if (i != 2 || i != 4)
+    {
+      printf("%d", i);
+    }
+  }
+}
