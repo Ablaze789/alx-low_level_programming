2e3f85aa170b1463b9312e4c078a34d0e2059a96
Ablaze789_ALX <anthonychinaza9@gmail.com>
2022-10-21 05:50:51 -0700

    Update

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
-*	print_most_numbers - This prints the mumber 0-9
+*	prin_numbers - This prints the mumber 0-9
 *	Return: void
 */
 
-void print_most_numbers(void)
+void print_numbers(void)
 {
 	char i;
 
