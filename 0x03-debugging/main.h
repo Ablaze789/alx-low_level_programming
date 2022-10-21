88e624d81e453641a6df85f2b3a814dde82ed227
Anthony <75227567+Ablaze789@users.noreply.github.com>
2022-10-21 09:05:22 +0100

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -8,14 +8,6 @@
  */
 
 void positive_or_negative(int i)
-{
-	if (i > 0)
-		printf("%d is positive\n", i);
-	else
-		if (i == 0)
-			printf("%d is zero\n", i);
-		else
-			printf("%d is negative\n", i);
-}
+int largest_number(int a, int b, int c)
 
 #endif /*MAIN_H*/
