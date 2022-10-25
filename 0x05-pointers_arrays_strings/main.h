d94ce0a8370ce7c0a7344498c754ecb8b7ceac33
Anthony <75227567+Ablaze789@users.noreply.github.com>
2022-10-25 12:36:55 +0100

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,11 +2,8 @@
 #define MAIN_H
 
 #include <stdio.h>
-<<<<<<< HEAD
 #include <string.h>
-=======
 #include <stdlib.h>
->>>>>>> 062fe5832bab527763572578a841368c3f58d4a4
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
