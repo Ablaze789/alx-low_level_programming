a48ec6bed0072290eb467fa377014fdefe15e66c
Ablaze789_ALX <anthonychinaza9@gmail.com>
2022-10-24 23:08:46 -0700

    Update

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *	_strlen - This prints the length of a string
+ *	@s: This holds the address of the string
+ *	Return: Always 0
+ */
+
+int _strlen(char *s)
+{
+	int len = strlen(s);
+	return (len);
+}
