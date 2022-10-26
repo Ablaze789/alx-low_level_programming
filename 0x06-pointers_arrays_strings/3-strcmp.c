9790dd43aed26bf748aa449e2a1f641b1a062759
Anthony <75227567+Ablaze789@users.noreply.github.com>
2022-10-26 22:14:50 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,17 +10,14 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i;
-	int j;
-	int n;
 
 	i = 0;
-	j = 0;
 
 	while (s1[i] != '\0' && s2[j] != '\0')
 	{
 		if (s1[i] != s2[i])
 		{
-			return (s1[1] - s2[i]);
+			return (s1[i] - s2[i]);
 		}
 		i++;
 	}
