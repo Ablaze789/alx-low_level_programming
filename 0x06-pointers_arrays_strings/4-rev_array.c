43b792c280627a470cc0904d1750830658000544
Anthony <75227567+Ablaze789@users.noreply.github.com>
2022-10-26 22:47:54 +0100

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -5,7 +5,7 @@
  *	content of an array of integers.
  *	@a: array to be reversed
  *	@n: number of elements of the array
- *	Return: Always 0
+ *	Return: Void
  */
 
 void reverse_array(int *a, int n)
