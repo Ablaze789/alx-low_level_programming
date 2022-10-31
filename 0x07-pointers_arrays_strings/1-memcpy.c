53373fd845396b59b1378a2cf6e247a34d930745
Anthony <75227567+Ablaze789@users.noreply.github.com>
2022-10-31 18:29:14 +0100

    Update 1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *      *_memset - The _memset() function fills the first n bytes of
+ *      the memory area pointed to by s with the constant byte b.
+ *      @dest: The destination memory area
+ *      @src: The source memory address
+ *      @n: The no of byte to be replaced
+ *      Return: Char
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+		int i;
+
+		i = 0;
+		while (i < n)
+		{
+			dest[i] = src[i];
+			i++;
+		}
+}
