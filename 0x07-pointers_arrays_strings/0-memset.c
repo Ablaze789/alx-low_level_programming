12ecc8a9aa781e3756971f530674c04b4f4684fb
Anthony <75227567+Ablaze789@users.noreply.github.com>
2022-11-01 23:35:18 +0100

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *	*_memset - The _memset() function fills the first n bytes of
+ *	_memset - The _memset() function fills the first n bytes of
  *	the memory area pointed to by s with the constant byte b.
  *	@s: The memory area
- *	@b: The constantb byte
+ *	@b: The constant byte
  *	@n: The no of byte to be replaced
  *	Return: Char
  */
