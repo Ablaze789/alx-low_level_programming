a0e516fdfb5fae0443e7605c1f9792c4d6c23f00
Anthony <75227567+Ablaze789@users.noreply.github.com>
2022-11-01 23:55:04 +0100

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * _strchr - This function a pointer to the first occurrence of the character
- * c in the string s, or NULL if the character is not found
- * @s: Reference string
- * @c: input char
- * Return: Char
+ *	_strchr - This function a pointer to the first occurrence of the character
+ *	c in the string s, or NULL if the character is not found
+ *	@s: Reference string
+ *	@c: input char
+ *	Return: Char
  */
 
 char *_strchr(char *s, char c)
