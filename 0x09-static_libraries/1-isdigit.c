f8862191d8ee3534704dd1eec8f7ce370cb4e099
Anthony <75227567+Ablaze789@users.noreply.github.com>
2022-11-05 19:48:25 +0100

    Update 1-isdigit.c

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *	_isdigit - a function that prints a string, followed by a new line.
+ *	@c: string to print
+ *	Return: void
+ */
+
+int _isdigit(int c)
+{
+	return (0);
+}
