0eb93c970fc8e5677fbe98c5304fe707d1e9a340
Anthony ALX Project <anthonychinaza9@gmail.com>
2022-11-04 17:08:16 -0700

    Update

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ *	main - This is the program entry point
+ *	@argc: This the length of the argv array
+ *	@argv: This is the array that contains the command line arguments
+ *	Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d", argc);
+	return (0);
+}
