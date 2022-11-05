233bc85c9de8446992cdd053abb4142c71a5c35b
Anthony <75227567+Ablaze789@users.noreply.github.com>
2022-11-05 05:22:23 +0100

    Update 0-strcat.c

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *	_strcat - a function that prints a string, followed by a new line.
+ *	@c: string to print
+ *	Return: void
+ */
+
+int char *_strcat(char *dest, char *src)
+{
+	return (0);
+}
