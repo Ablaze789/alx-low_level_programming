6de660a8f67fe4efee0e4224b24bb796b181f734
Anthony <75227567+Ablaze789@users.noreply.github.com>
2022-11-11 08:42:58 +0100

    Update 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *	_strcat - This function appends the src string to the dest string
+ *	@dest: This hold the first string
+ *	@src: This hold the second string
+ *	Return: Always 0
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+	return (dest);
+}
