9b7d9bb271145660f6ca95cf66a1f0fb6b6aee5e
Anthony <75227567+Ablaze789@users.noreply.github.com>
2022-11-11 09:10:56 +0100

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,19 +1,59 @@
 #include "main.h"
 
 /**
- *	malloc_checked - This is a function that allocates memory using malloc
+ *	string_nconcat - This is a a function that concatenates two strings.
  *	@b: size to be allocated
  *	Return: NULL
  */
 
-void *malloc_checked(unsigned int b)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	void *p;
+	unsigned int str_one;
+	unsigned int str_two;
+	unsigned int i;
 
-	p = malloc(b);
+	str_one = 0;
+	str_two = 0;
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+
+	while (s1[str_one] != '\0')
+	{
+		str_one++;
+	}
+
+	while (s1[str_two] != '\0')
+	{
+		str_two++;
+	}
+
+	if (n > str_two)
+	{
+		n = str_two;
+		p = malloc((size1 + n + 1) * sizeof(char));
+	}
+
 	if (p == NULL)
 	{
-		exit(98);
+		return (0);
+	}
+
+	for (i = 0; i < str_one; i++)
+	{
+		p[i] = s1[i];
+	}
+
+	for (; i < str_two; i++)
+	{
+		p[i] = s2[i - str_one];
 	}
 
 	return (p);
