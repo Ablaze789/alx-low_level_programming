1496f18d924685684a67e0e31896e48190f22321
Anthony <75227567+Ablaze789@users.noreply.github.com>
2022-11-16 08:56:25 +0100

    Create 1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ *	array_iterator - Execute a function for each element of an arrray
+ *	@array: array of numbers
+ *	@size: size of the array to convert
+ *  @action: The pointer to the function to execute
+ *	Return: Void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+  i = 0;
+	while (i < size)
+	{
+		action(array[i]);
+		i++;
+  }
+}
