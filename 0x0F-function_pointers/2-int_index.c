2454ae388dde687dceaf6f34395e5dc15801ac69
Anthony <75227567+Ablaze789@users.noreply.github.com>
2022-11-16 11:11:36 +0100

    Create 2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ *	array_iterator - Execute a function for each element of an arrray
+ *	@array: array of numbers
+ *	@size: size of the array to convert
+ *	@cmp: compares elements of array with a given integer
+ *	Return: Integer
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	i = 0;
+	if (size <= 0)
+  {
+		return (-1);
+  }
+  
+	while (i < size)
+	{
+		if (cmp(array[i]))
+		{
+			return (i);
+		}
+		i++;
+	}
+	return (-1)
+}
