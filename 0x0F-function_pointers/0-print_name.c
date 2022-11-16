64cfb191d41d605c8553626416c021eb630d4831
Anthony ALX Project <anthonychinaza9@gmail.com>
2022-11-15 23:32:57 -0800

    Create file

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ *	print_name - This function prints the name inputted
+ *	@name: name to be printed
+ *	@f: Format to print name
+ *	Return: Void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
